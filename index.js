91eeb8d9b25aaff68b152ebdc778b100c98dbbea
Shantanu Bajaj <shantanubajaj05@gmail.com>
2022-06-20 16:21:12 +0530

    Created route for new user registration

--- index.js
+++ index.js
@@ -28,6 +28,34 @@ app.get("/allcharacters", (req, res) => {
     });
 });
 
+app.post("/user/register", (req, res) => {
+  var sqll = "SELECT email FROM users WHERE email='" + req.query.email + "'";
+con.query(sqll, function (err, result) {
+  if (err) throw err;
+  if (!result.length) {
+      if(req.query.password.length < 6) {
+          res.send({err:"Password length should be at least 6 characters"});
+      }
+      else{
+          var sql = 
+            "INSERT INTO users (name, email, password) VALUES ('" +
+            req.query.name +
+            "','" +
+            req.query.email +
+            "','" +
+            req.query.password +
+            "')";
+          con.query(sql, function (err, results) {
+            if (err) throw err;
+            res.send({ message: "User Registered successfully" });
+          });
+      }
+  } else {
+    res.send({ message: "Email already exists" });
+  }
+});
+});
+
 
 
 
